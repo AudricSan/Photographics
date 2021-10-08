6c48e6912ac0bda226500082131f7ba6b14150bb
Audric Rosier <audricrosier@gmail.com>
2021-10-08 09:47:13 +0200

    Add Basic Files
    - Footer.html
    - Index to redirect correcly
    - Index.php
    - Header.php

---
+++ Project - PublicSide/view/php/include/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Photographics - TFE</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="A photo website for anyone">
+    <meta name="author" content="Audric Rosier">
+
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le styles -->
+
+    <!-- Fav and touch icons -->
+
+</head>
+
+<body>
+    <div class="navbar navbar-inverse">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="brand" href="http://localhost/penpal">Penpal</a>
+
+                <div class="nav-collapse collapse">
+                    <ul class="nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+
+                    <ul>
+                        <li><a href="register.php">Register</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="container">

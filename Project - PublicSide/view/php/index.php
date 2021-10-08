671968616634a2e72fdb57705ace859ce54ec96b
Audric Rosier <audricrosier@gmail.com>
2021-10-08 09:47:13 +0200

    Add Basic Files
    - Footer.html
    - Index to redirect correcly
    - Index.php
    - Header.php

---
+++ Project - PublicSide/view/php/index.php
@@ -0,0 +1,13 @@
+<?php
+//Import Header
+require('include/header.php'); ?>
+
+
+<h1> COUCOU </h1>
+<p> Ceci est un paragraphe</p>
+
+
+<?php
+//Import Footer
+require('../html/include/footer.html');
+?>

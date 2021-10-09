fc70291eb08a07884a24f81b26857170083d5469
Audric Rosier <audricrosier@gmail.com>
2021-10-09 22:29:26 +0200

    Update index.php
    - Add generate File if tags exist and file not

--- Project - PublicSide/index.php
+++ Project - PublicSide/index.php
@@ -1,3 +1,28 @@
 <?php
-    header('location: view/php/index.php');
+    //Include
+        require('model/read.php');
+        require('model/function.php');
+    //
+    
+    //Check if page of tags Exist, If not create new page with code
+        $alltags  = takeNameTag();
+        foreach ($alltags as $key => $value){
+            if(!file_exists("view/php/$value.php")){
+                //Check Position
+                    echo "File Not Exist";
+                    var_dump($value);
+                //
+                
+                $newpage = fopen("view/php/$value.php", "w") or die("Unable to open file!");
+                $basiccode  = file_get_contents("model/basic.txt");
+            
+                fwrite($newpage, $basiccode);
+                fclose($newpage);
+            }
+        }
+    //
+
+    // Redirecto to Correct Index    
+        header('location: view/php/index.php');
+    //
 ?>

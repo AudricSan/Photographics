b3b5016e9fa5f3073d6f332e3a4533e920616adc
Audric Rosier <audricrosier@gmail.com>
2021-10-11 01:15:41 +0200

    Update index.php
    - add check if there are tags before try to create files
    - delete files if there are a file but not the tags

--- Project - PublicSide/index.php
+++ Project - PublicSide/index.php
@@ -6,18 +6,37 @@
 
     //Check if page of tags Exist, If not create new page with code
         $alltags  = takeNameTag();
-        foreach ($alltags as $key => $value){
-            if(!file_exists("view/php/$value.php")){
-                //Check Position
-                    echo "File Not Exist";
-                    var_dump($value);
-                //
 
-                $newpage = fopen("view/php/$value.php", "w") or die("Unable to open file!");
-                $basiccode  = file_get_contents("model/basic.txt");
+        if($alltags != false){
+            foreach ($alltags as $key => $value){
+                if(!file_exists("view/php/$value.php")){
+                    //Check Position
+                        echo "Create File for Tags" . '<br>';
+                        var_dump($value);
+                    //
 
-                fwrite($newpage, $basiccode);
-                fclose($newpage);
+                    $newpage = fopen("view/php/$value.php", "w") or die("Unable to open file!");
+                    $basiccode  = file_get_contents("model/basic.txt");
+
+                    fwrite($newpage, $basiccode);
+                    fclose($newpage);
+                }                        
+            }
+        }
+
+        $existFile = scandir("view/php/", $sorting_order = SCANDIR_SORT_ASCENDING);
+        $dir = "view/php/";
+
+        foreach ($existFile as $key2 => $value2) {
+            if ($value2 == "index.php" || $value2 == "include" || $value2 == "." || $value2 == "..") {
+                echo "PAS TOUCHE" . '<br>';
+            } else {
+                $value2 = explode('.', $value2);
+                $value2 = $value2[0];
+                if (!in_array($value2, $alltags)) {
+                    echo 'file delete cuz not exist in DB' . '<br>';
+                    array_map('unlink', glob("{$dir}*$value2.php"));
+                }
             }
         }
     //

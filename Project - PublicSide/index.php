9250f1479f6ff7ac65b01b6c62074a1ccabd85dc
Audric Rosier <audricrosier@gmail.com>
2021-10-11 00:07:16 +0200

    Update index.php

--- Project - PublicSide/index.php
+++ Project - PublicSide/index.php
@@ -3,7 +3,7 @@
         require('model/read.php');
         require('model/function.php');
     //
-    
+
     //Check if page of tags Exist, If not create new page with code
         $alltags  = takeNameTag();
         foreach ($alltags as $key => $value){
@@ -12,17 +12,17 @@
                     echo "File Not Exist";
                     var_dump($value);
                 //
-                
+
                 $newpage = fopen("view/php/$value.php", "w") or die("Unable to open file!");
                 $basiccode  = file_get_contents("model/basic.txt");
-            
+
                 fwrite($newpage, $basiccode);
                 fclose($newpage);
             }
         }
     //
 
-    // Redirecto to Correct Index    
+    // Redirecto to Correct Index
         header('location: view/php/index.php');
     //
 ?>

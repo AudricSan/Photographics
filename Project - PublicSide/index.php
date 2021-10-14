94d29eb5397e9036f2ba67ede3bf3d4dd674fe73
unknown <audricrosier@gmail.com>
2021-10-14 17:03:12 +0200

    Update index.php

--- Project - PublicSide/index.php
+++ Project - PublicSide/index.php
@@ -7,23 +7,21 @@
     //Check if page of tags Exist, If not create new page with code
         $alltags  = takeNameTag();
 
-        if($alltags != false){
-            foreach ($alltags as $key => $value){
-                if(!file_exists("view/php/$value.php")){
-                    //Check Position
-                        echo "Create File for Tags" . '<br>';
-                        var_dump($value);
-                    //
+        if ($alltags != false) {
+            foreach ($alltags as $key => $value) {
+                if (!file_exists("view/php/$value.php")) {
 
                     $newpage = fopen("view/php/$value.php", "w") or die("Unable to open file!");
                     $basiccode  = file_get_contents("model/basic.txt");
 
                     fwrite($newpage, $basiccode);
                     fclose($newpage);
-                }                        
+                }
             }
         }
+    //
 
+    // If tag not Exist Remove files
         $existFile = scandir("view/php/", $sorting_order = SCANDIR_SORT_ASCENDING);
         $dir = "view/php/";
 
@@ -36,7 +34,6 @@
                 if (!in_array($value2, $alltags)) {
                     echo 'file delete cuz not exist in DB' . '<br>';
                     array_map('unlink', glob("{$dir}*$value2.php"));
-                }
             }
         }
     //

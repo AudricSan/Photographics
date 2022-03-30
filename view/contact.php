72203f99bd521bf146d556bb2aa3c401588bbc7a
Audric Rosier <audricrosier@gmail.com>
2022-03-30 15:40:41 +0200

    Create contact.php

---
+++ view/contact.php
@@ -0,0 +1,29 @@
+<section class='contact'>
+<?php
+
+
+  echo "
+        <div class='contact'>
+            <h2>Title</h2>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. amet consectetur adipisicing.</p>
+
+            <form class='generated-form'  method='GET' action='submit.php'  target='_self'>
+                <label for='name'> Name:</label>
+                <input type='name' id='name' name='name'>
+
+                <label for='mail'> Mail:</label>
+                <input type='mail' id='mail' name='mail'>
+
+                <label for='sujet'> Sujet:</label>
+                <input type='sujet' id='sujet' name='sujet'>
+
+                <label for='message'>Votre message:</label>
+                <textarea id='message' name='message' rows='2' cols='5'></textarea>
+
+                <input type='submit' value='Submit'>
+            </form>
+
+        </div>
+  ";
+?>
+</section>

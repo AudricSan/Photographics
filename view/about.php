12e73ef4a71c18245bc0a1898b0a76ba05e546ba
Audric Rosier <audricrosier@gmail.com>
2022-03-30 14:57:41 +0200

    Create about.php

---
+++ view/about.php
@@ -0,0 +1,31 @@
+<section class='about'>
+<?php
+
+
+  echo "  <div class='about'>
+            <h2>Title</h2>
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente similique ipsum debitis nemo quam,
+                dolor unde. Magni mollitia similique tenetur nisi minima? Quo accusantium alias ad magni porro
+                doloremque natus? </p>
+
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente similique ipsum debitis nemo quam,
+                dolor unde. Magni mollitia similique tenetur nisi minima? Quo accusantium alias ad magni porro
+                doloremque natus? </p>
+
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente similique ipsum debitis nemo quam,
+                dolor unde. Magni mollitia similique tenetur nisi minima? Quo accusantium alias ad magni porro
+                doloremque natus? </p>
+
+            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente similique ipsum debitis nemo quam,
+                dolor unde. Magni mollitia similique tenetur nisi minima? Quo accusantium alias ad magni porro
+                doloremque natus? </p>
+
+            <a href='#'> <i class='fa-brands fa-facebook'></i> </a>
+            <a href='#'> <i class='fa-brands fa-instagram'></i> </a>
+            <a href='#'> <i class='fa-brands fa-twitter'></i> </a>
+            <a href='#'> <i class='fa-brands fa-twitch'></i> </a>
+          </div>
+  ";
+?>
+
+</section>

b4125c9fbdff56db7a6112123fe8f2f51bb4c0fb
Audric Rosier <audricrosier@gmail.com>
2022-03-31 17:22:02 +0200

    Update index.php

--- view/admin/index.php
+++ view/admin/index.php
@@ -1,134 +1,52 @@
 <main class="dashboard">
-        <nav class="lateral">
-            <ul>
-                <li class='latnav selected'>
-                    <span class="material-icons-round">dashboard</span>
-                    <a href='/'>Gallery</a>
-                </li>
+    <nav class="lateral">
+        <ul>
+            <li class='latnav selected'>
+                <span class="material-icons-round">dashboard</span>
+                <a href='/'>Gallery</a>
+            </li>
 
-                <li class='latnav'>
-                    <span class="material-icons-round">image</span>
-                    <a href='/'>Gallery</a>
-                </li>
+            <li class='latnav'>
+                <span class="material-icons-round">image</span>
+                <a href='/'>Gallery</a>
+            </li>
 
-                <li class='latnav'>
-                    <span class="material-icons-round">local_offer</span>
-                    <a href='/'>Gallery</a>
-                </li>
+            <li class='latnav'>
+                <span class="material-icons-round">local_offer</span>
+                <a href='/'>Gallery</a>
+            </li>
 
-                <li class='latnav'>
-                    <span class="material-icons-round">admin_panel_settings</span>
-                    <a href='/'>Gallery</a>
-                </li>
+            <li class='latnav'>
+                <span class="material-icons-round">admin_panel_settings</span>
+                <a href='/'>Gallery</a>
+            </li>
 
-                <li class='latnav'>
-                    <span class="material-icons-round">api</span>
-                    <a href='/'>Gallery</a>
-                </li>
+            <li class='latnav'>
+                <span class="material-icons-round">api</span>
+                <a href='/'>Gallery</a>
+            </li>
 
-                <li class='latnav'>
-                    <span class="material-icons-round">class</span>
-                    <a href='/'>Gallery</a>
-                </li>
+            <li class='latnav'>
+                <span class="material-icons-round">class</span>
+                <a href='/'>Gallery</a>
+            </li>
 
-                <li class='latnav'>
-                    <span class="material-icons-round">bug_report</span>
-                    <a href='/'>Gallery</a>
-                </li>
-            </ul>
-        </nav>
+            <li class='latnav'>
+                <span class="material-icons-round">bug_report</span>
+                <a href='/'>Gallery</a>
+            </li>
+        </ul>
+    </nav>
 
-        <div class="dashboard">
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
+    <div class="dashboard">
+        <div class="dash-content">
+            <h2>Title</h2>
+            <h3>Desctipyion</h3>
 
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
+            <div>
+                <a class="btn disable" href=""> More option </a>
+                <a class="btn validate" href="/"> More option </a>
             </div>
-
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
-
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
-            </div>
-
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
-
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
-            </div>
-
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
-
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
-            </div>
-
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
-
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
-            </div>
-            
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
-
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
-            </div>
-
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
-
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
-            </div>
-
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
-
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
-            </div>
-
-            <div class="dash-content">
-                <h2>Title</h2>
-                <h3>Desctipyion</h3>
-
-                <div>
-                    <a class="btn disable" href=""> More option </a>
-                    <a class="btn validate" href="/"> More option </a>
-                </div>
-            </div>
-
         </div>
-
-    </main>
+    </div>
+</main>

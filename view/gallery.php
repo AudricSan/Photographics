a250fb201a75d0de484f0b58c18a11afa13f50ce
Audric Rosier <audricrosier@gmail.com>
2022-04-04 09:58:54 +0200

    Update gallery.php

--- view/gallery.php
+++ view/gallery.php
@@ -1,15 +1,16 @@
 <section>
-  <?phP
+  <?php
 
   if (!empty($_GET['id'])) {
     $tagDao = new TagDAO;
     $tag = $tagDao->fetch($_GET['id']);
-
     $title = $tag->_name;
     $subtitle = "Some of my $tag->_name";
-  }else{
+
+  } else {
     $title = 'Gallery';
     $subtitle = 'Some of my Work';
+
   }
 
   echo "
@@ -22,14 +23,11 @@
   echo "<div class='gallery'>";
 
   $pictureDAO = new PictureDAO;
-  $pictureTagDAO = new PictureTagDAO; 
+  $pictureTagDAO = new PictureTagDAO;
 
   if (!empty($_GET['id'])) {
     $pictureTag = $pictureTagDAO->fetch($_GET['id']);
-    // var_dump($pictureTag);
-
     foreach ($pictureTag as $key => $value) {
-      // var_dump($value->_pic);
       $picture = $pictureDAO->fetch($value->_pic);
 
       echo "<div class='media'>
@@ -38,29 +36,34 @@
   
             <div>
               <a href='#'>
-                <i class='fa-solid fa-heart'></i></a>
-              <a href='#'>
-                <i class='fa-solid fa-share-nodes'></i></a>
-            </div>
-          </div>";
+                <i class='fa-solid fa-heart'></i></a>";
+
+      if ($picture->_sharable) {
+        echo "<a href='#'>
+        <i class='fa-solid fa-share-nodes'></i></a>";
+      }
+
+      echo "</div></div>";
     }
-
-
-  }else{
+  } else {
     $picture = $pictureDAO->fetchAll();
-    foreach ($picture as $picture) {  
+    foreach ($picture as $picture) {
       $pic = $pictureDAO->fetch($picture->_id);
+
       echo "<div class='media'>
             <a href='#'>
               <img src='/public/img/img/$pic->_link' alt='$pic->_name'></a>
   
-            <div>
+              <div>
               <a href='#'>
-                <i class='fa-solid fa-heart'></i></a>
-              <a href='#'>
-                <i class='fa-solid fa-share-nodes'></i></a>
-            </div>
-          </div>";
+                <i class='fa-solid fa-heart'></i></a>";
+
+      if ($picture->_sharable) {
+        echo "<a href='#'>
+        <i class='fa-solid fa-share-nodes'></i></a>";
+      }
+
+      echo "</div></div>";
     }
   }
 

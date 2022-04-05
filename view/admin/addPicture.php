f05333c87bc76daab4095960b758a05d7f88a3aa
Audric Rosier <audricrosier@gmail.com>
2022-04-05 15:01:00 +0200

    Update addPicture.php

--- view/admin/addPicture.php
+++ view/admin/addPicture.php
@@ -1,35 +1,38 @@
 <?php
 
-$imgroot = $_SESSION['imgroot'];
+$tagDao = new TagDAO;
+$tags = $tagDao->fetchAll();
+
 
 echo "
     <div class='dashboard'>
-        <form method='POST' action='' enctype='multipart/form-data' target='_self'>
+        <form method='POST' action='/admin/picture/create' enctype='multipart/form-data' target='_self'>
             <label for='title'>Image Title :</label>
-            <input type='text' id='title' name='title'>
+            <input type='text' id='title' name='title' required>
 
             <label for='desc'>Image Description :</label>
-            <input type='text' id='desc' name='desc'>
+            <input type='text' id='desc' name='desc' required>
 
             <div>
                 <p>Select Tags : </p>";
-
-                
-                // <div>
-                //     <label for='tags1'>Tags1</label>
-                //     <input type='checkbox' id='tags1' name='tags1' value='tags1'>
-                // </div>
+                foreach ($tags as $tag){
+                    echo "
+                        <div>
+                            <label for='$tag->_id'>$tag->_name</label>
+                            <input type='checkbox' id='$tag->_id' name='tag_$tag->_id' value='$tag->_id'>
+                        </div>";
+                }
 
 echo "
             </div>
 
             <label for='share'>Sharable</label>
-            <input type='checkbox' class='switch' id='share' name='share' value='1'>
+            <input type='checkbox' class='switch' id='share' name='share'>
 
             <label for='file'>File:</label>
-            <input type='file' id='file' name='file'>
+            <input type='file' id='file' name='file' required>
 
-            <input class='btn validate' type='submit' value='Submit'>
+            <input class='btn validate' type='submit' value='Submit' required>
         </form>
     </div>
 </div></main>";

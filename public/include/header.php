10ce7203ae4d7581e11f5f618ce0c845ee554366
Audric Rosier <audricrosier@gmail.com>
2022-04-05 11:40:47 +0200

    Update header.php
    correct link for favicon

--- public/include/header.php
+++ public/include/header.php
@@ -76,11 +76,11 @@ echo "
         <meta name='description' content='$description'>
         <meta name='auteur' content='$autor'>
 
-        <link rel='apple-touch-icon' sizes='180x180' href='img/ico/apple-touch-icon.png'>
-        <link rel='icon' type='image/png' sizes='32x32' href='img/ico/favicon-32x32.png'>
-        <link rel='icon' type='image/png' sizes='16x16' href='img/ico/favicon-16x16.png'>
-        <link rel='manifest' href='img/ico/site.webmanifest'>
-        <link rel='mask-icon' href='img/ico/safari-pinned-tab.svg' color='#5bbad5'>
+        <link rel='apple-touch-icon' sizes='180x180' href='$imglink/ico/apple-touch-icon.png'>
+        <link rel='icon' type='image/png' sizes='32x32' href='$imglink/ico/favicon-32x32.png'>
+        <link rel='icon' type='image/png' sizes='16x16' href='$imglink/ico/favicon-16x16.png'>
+        <link rel='manifest' href='$imglink/ico/site.webmanifest'>
+        <link rel='mask-icon' href='$imglink/ico/safari-pinned-tab.svg' color='#5bbad5'>
         <meta name='apple-mobile-web-app-title' content='Photographics'>
         <meta name='application-name' content='Photographics'>
         <meta name='msapplication-TileColor' content='#ffffff'>

7526c3c3e2d862f7993a06135675bde258729fe6
Audric Rosier <audricrosier@gmail.com>
2022-04-05 17:38:37 +0200

    Update addPicture.php

--- view/admin/addPicture.php
+++ view/admin/addPicture.php
@@ -1,17 +1,34 @@
 <?php
 
+use photographics\PictureTag;
+
 $tagDao = new TagDAO;
 $tags = $tagDao->fetchAll();
 
+if(isset($id)){
+    $pictureDAO = new PictureDAO;
+    $picture = $pictureDAO->fetch($id);
+
+    $pictureTagDAO = new PictureTagDAO;
+    $pictureTags = $pictureTagDAO->fetchByPic($id);
+
+    $imgroot = $_SESSION['imgroot'];
+}
 
 echo "
     <div class='dashboard'>
-        <form method='POST' action='/admin/picture/create' enctype='multipart/form-data' target='_self'>
+        <form method='POST' action='";
+        if(isset($id)){
+            echo"/admin/picture/edit";
+        }else{
+            echo"/admin/picture/create";
+        }
+        echo"'enctype='multipart/form-data' target='_self'>
             <label for='title'>Image Title :</label>
-            <input type='text' id='title' name='title' required>
+            <input type='text' id='title' name='title'"; if (isset($picture)) {echo "value = '$picture->_name'";} echo"required>
 
             <label for='desc'>Image Description :</label>
-            <input type='text' id='desc' name='desc' required>
+            <input type='text' id='desc' name='desc'"; if (isset($picture)) { echo "value = '$picture->_description'";} echo"required>
 
             <div>
                 <p>Select Tags : </p>";
@@ -19,20 +36,31 @@ echo "
                     echo "
                         <div>
                             <label for='$tag->_id'>$tag->_name</label>
-                            <input type='checkbox' id='$tag->_id' name='tag_$tag->_id' value='$tag->_id'>
-                        </div>";
+                            <input type='checkbox' id='$tag->_id' name='tag_$tag->_id' value='$tag->_id'"; 
+
+                            foreach ($pictureTags as $pictureTag){
+                                if($pictureTag->_tag == $tag->_id){
+                                    echo "checked";
+                                } 
+                            }
+                        echo"></div>";
                 }
 
 echo "
             </div>
 
             <label for='share'>Sharable</label>
-            <input type='checkbox' class='switch' id='share' name='share'>
+            <input type='checkbox' class='switch' id='share' name='share'";if (isset($picture)) {if ($picture->_sharable === 1){echo 'checked';}}echo ">";
 
-            <label for='file'>File:</label>
-            <input type='file' id='file' name='file' required>
+            if (!isset($picture)) {
+                echo "
+                <label for='file'>File:</label>
+                <input type='file' id='file' name='file' required>";
+            }else{
+                echo "<div class='img'><img src='$imgroot/img/$picture->_link' ></div>";
+            }
 
+echo"
             <input class='btn validate' type='submit' value='Submit' required>
-        </form>
-    </div>
-</div></main>";
+        </form>";
+echo"</div></div></main>";

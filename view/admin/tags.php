c0839ce9d8d287ec3f9267fd656255c25c405ff6
Audric Rosier <audricrosier@gmail.com>
2022-04-06 09:53:13 +0200

    Create tags.php

---
+++ view/admin/tags.php
@@ -0,0 +1,38 @@
+<?php
+
+$imgroot = $_SESSION['imgroot'];
+
+echo "<div class='dashboard'>
+    <h1> <a href='/admin/tag/add' class='btn additems success'> <span class='material-icons-round'> add </span> </a> </h1>
+
+    <table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Quick Action</th>
+        </tr>
+    </thead>
+";
+
+$tagDAO = new TagDAO;
+$tags = $tagDAO->fetchAll();
+
+foreach ($tags as $key => $tag) {
+    echo "
+    <tbody>
+        <tr>
+            <td> $tag->_name </td>
+            <td> $tag->_description </td>
+
+            <td class=action with=500>
+                <a class='btn validate' href='#'>See</a>
+                <a class='btn success' href='/admin/tags/add/$tag->_id'>Edit</a>
+                <a class='btn error' href='#'>Delete</a>
+            </td>
+        </tr>
+    </tbody>
+";
+}
+
+echo "</table></div></main>";

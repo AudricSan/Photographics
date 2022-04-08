6fc810dde2e439e340c3c9cfd2fa55781c7624d2
Audric Rosier <audricrosier@gmail.com>
2022-04-08 23:37:31 +0200

    Update love.js

--- public/js/love.js
+++ public/js/love.js
@@ -1,7 +1,20 @@
 console.log('hello World, Love');
 
-function love(id){
+function love(elem){
     console.log('function love');
-    id = id.id;
+    id = elem.id;
+    console.log(id);
+    console.log(elem);
 
+    // child = document.createElement("p");
+    // child.innerHTML = '+1'
+    // child.classList.add('loveAlert');
+    // elem.appendChild(child);
+
+    elem.classList.add('loveAlert')
+
+    setTimeout(function() {
+        // child.remove();
+        elem.classList.remove('loveAlert')
+    }, 3000);
 }

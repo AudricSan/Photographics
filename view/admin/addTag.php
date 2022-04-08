425f2eb7bfe9ae7ec94a87254c9e85cfe6465633
Audric Rosier <audricrosier@gmail.com>
2022-04-08 16:30:59 +0200

    Tags
    - add and edit tags

---
+++ view/admin/addTag.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($id)){
+    $tagDao = new TagDAO;
+    $tag = $tagDao->fetch($id);
+}
+echo "
+    <div class='dashboard'>
+        <form method='POST' action='";
+
+        if(isset($tag)){
+            echo"/admin/tag/edit";
+        }else{
+            echo"/admin/tag/create";
+        }
+
+        echo"'enctype='multipart/form-data' target='_self'>
+            <label for='title'>Tag Title :</label>
+            <input type='text' id='title' name='title'"; if (isset($tag)) {echo "value = '$tag->_name'";} echo"required>
+
+            <label for='desc'>Tag Description :</label>
+            <input type='text' id='desc' name='desc'"; if (isset($tag)) { echo "value = '$tag->_description'";} echo"required>";
+        
+            if (isset($tag)) {
+               echo "<input type='number' name='tag_id' value='$tag->_id' style='display:none'></input>";
+            }
+
+        echo"<input class='btn validate' type='submit' value='Submit' required>
+        </form>";
+echo"</div></div></main>";

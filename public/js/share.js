309cca8b28b241eb72083572ccc494ae8935c4b8
Audric Rosier <audricrosier@gmail.com>
2022-04-08 22:52:39 +0200

    Update share.js

--- public/js/share.js
+++ public/js/share.js
@@ -1,1 +1,9 @@
-console.log('hello World, Share')
+console.log('hello World, Share');
+
+function share(id){
+    console.log('Share share');
+    id = id.id;
+    link = 'http://127.0.0.111/see/' + id;
+    navigator.clipboard.writeText(link);
+    alert('link sarable copy to clipboard');
+}

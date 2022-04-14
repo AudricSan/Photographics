8a22832cd7938e3784063d887c36a38484838aee
Audric Rosier <audricrosier@gmail.com>
2022-04-14 14:49:19 +0200

    Update dump sql.sql

--- model/slq/dump sql.sql
+++ model/slq/dump sql.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `admin` ADD `admin_login` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER `admin_role`;
-
 INSERT INTO `role` (`role_id`, `role_name`) VALUES
                    (NULL, 'admin'),
                    (NULL, 'editor'),

578e12e84f8ffba9efca5c2f48dd2a804c8760c9
Audric Rosier <audricrosier@gmail.com>
2022-04-14 10:33:30 +0200

    Update about.php

--- view/about.php
+++ view/about.php
@@ -1,6 +1,8 @@
 <section class='about'>
     <?php
 
+use photographics\BasicInfo;
+
     $root = $_SESSION['root'];
 
     $photographerName = $_SESSION['basicinfo']['Photographer Name'];
@@ -20,26 +22,48 @@
     if ($_SESSION['basicinfo']['facebook'] != 'NULL') {
         echo "<a href='https://www.facebook.com/" . $_SESSION['basicinfo']['facebook']  . "' target='_BLANK'> <i class='fa-brands fa-facebook'></i> </a>";
     }
-    
+
     if ($_SESSION['basicinfo']['instagram'] != 'NULL') {
         echo "<a href='https://www.instagram.com/" . $_SESSION['basicinfo']['instagram']  . "' target='_BLANK'> <i class='fa-brands fa-instagram'></i> </a>";
     }
-    
-    if ($_SESSION['basicinfo']['twitch'] != 'NULL')  {
+
+    if ($_SESSION['basicinfo']['twitch'] != 'NULL') {
         echo "<a href='https://www.twitch.tv/" . $_SESSION['basicinfo']['twitch']  . "' target='_BLANK'> <i class='fa-brands fa-twitch'></i> </a>";
     }
-    
-    if ($_SESSION['basicinfo']['twitter'] != 'NULL')  {
+
+    if ($_SESSION['basicinfo']['twitter'] != 'NULL') {
         echo "<a href='https://twitter.com/" . $_SESSION['basicinfo']['twitter']  . "' target='_BLANK'> <i class='fa-brands fa-twitter'></i> </a>";
     }
-    
-    if ($_SESSION['basicinfo']['linkedin'] != 'NULL')  {
+
+    if ($_SESSION['basicinfo']['linkedin'] != 'NULL') {
         echo "<a href='https://twitter.com/" . $_SESSION['basicinfo']['linkedin']  . "' target='_BLANK'> <i class='fa-brands fa-linkedin'></i> </a>";
     }
 
-    echo "
-        </div>
-";
-    ?>
+    echo "</div>"; 
 
+    $basicinfoDAO = new BasicInfoDAO;
+    $basicInfo = $basicinfoDAO->fetchAll();
+    // var_dump($basicInfo);
+    
+    foreach($basicInfo as $key => $value){        
+        if ($value->_name === 'About Picture') {
+            $pictureDAO = new PictureDAO; 
+            $picture = $pictureDAO->fetch($value->_content);
+        }
+    }
+
+    ?>
 </section>
+
+<script>
+    var ImgRoot = "<?php echo $_SESSION['imgroot']; ?>";
+    var img = "<?php echo $picture->_link; ?>";
+    const section = document.getElementsByTagName('section');
+
+    ImgRoot = ImgRoot + "/img/";
+    link = ImgRoot + img;
+
+    section[0].style.backgroundImage = 'url(' + link + ')';
+    console.log(section)
+    console.log(link)
+</script>

6fc4e7b62134b193f3014a7697db4a2e42a6ff56
Audric Rosier <audricrosier@gmail.com>
2022-05-11 14:57:14 +0200

    Update gallery.php
    - remove PictureTadDAO replace by a PictureByTag function

--- view/gallery.php
+++ view/gallery.php
@@ -22,12 +22,25 @@
 
   echo "<div class='gallery'>";
 
+  include_once('../model/class/Tag.php');
+  include_once('../model/dao/TagDAO.php');
+  $tagDAO = new TagDAO;
+  
+  include_once("../model/class/Picture.php");
+  include_once("../model/dao/PictureDAO.php");
+  $pictureDAO = new PictureDAO;
+  
+  include_once("../model/class/PictureTag.php");
+  include_once("../model/dao/PictureTagDAO.php");
+  $pictureTagDAO = new PictureTagDAO;
+
   $pictureDAO = new PictureDAO;
   
   if (!empty($_GET['id'])) {
-    $pictureTag = $pictureTagDAO->fetch($_GET['id']);
-    foreach ($pictureTag as $key => $value) {
-      $picture = $pictureDAO->fetch($value->_pic);
+    $pictureByTag = $pictureDAO->fetchByTag($_GET['id']);
+  
+    foreach ($pictureByTag as $key => $value) {
+      $picture = $pictureDAO->fetch($value['picture_id']);
 
       echo "<div class='media'>
             <a href='/see/$picture->_id'>
@@ -40,6 +53,7 @@
       if ($picture->_sharable) {
         echo "<a id='$picture->_id' onclick='share(this, event)'><i class='fa-solid fa-share-nodes'></i></a>";
       }
+
       echo "</div></div>";
     }
   } else {
@@ -48,7 +62,7 @@
       $pic = $pictureDAO->fetch($picture->_id);
 
       echo "<div class='media'>
-            <a href='/see/$picture->_id'>
+            <a href='/see/$pic->_id'>
               <img src='/public/images/img/$pic->_link' alt='$pic->_name'></a>
   
               <div>
